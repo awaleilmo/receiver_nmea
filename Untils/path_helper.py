361bdfe1682b1982e6e4f670c25bb602a3b5a87d
Dananjaya <danan12453@gmail.com>
2025-05-06 09:56:54 +0700

    added executables check

--- Untils/path_helper.py
+++ Untils/path_helper.py
@@ -5,21 +5,20 @@ import shutil
 
 def get_resource_path(relative_path, is_database=False):
     if is_database and relative_path.endswith('.db'):
-        if sys.platform.startswith('win'):
-            data_dir = os.path.join(os.environ['APPDATA'], 'SeaScope_Receiver')
-        else:
-            data_dir = os.path.join(os.path.expanduser('~'), '.SeaScope_Receiver')
+        #periksa jika berjalan untuk pyinstaller untuk executables jika tidak maka akan menggunakan path asli(current directory)
+        try:
+            if hasattr(sys, '_MEIPASS'):
+                exe_dir = os.path.dirname(sys.executable)
+            else:
+                exe_dir = os.path.abspath(".")
 
-        os.makedirs(data_dir, exist_ok=True)
+            os.makedirs(exe_dir, exist_ok=True)
 
-        persistent_path = os.path.join(data_dir, relative_path)
+            return os.path.join(exe_dir, relative_path)
+        except Exception as e:
+            print(f"Error setting database path: {str(e)}")
+            return os.path.join(os.path.abspath("."), relative_path)
 
-        if hasattr(sys, '_MEIPASS') and not os.path.exists(persistent_path):
-            bundled_db = os.path.join(sys._MEIPASS, relative_path)
-            if os.path.exists(bundled_db):
-                shutil.copy2(bundled_db, persistent_path)
-
-        return persistent_path
     try:
         base_path = sys._MEIPASS
     except AttributeError:
